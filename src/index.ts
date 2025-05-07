38044fcf75eefaac463f8fb295a87508d2c88d4f
gabrielalberini <gabialberini733@gmail.com>
2025-05-07 10:01:29 -0300

    incorporando conexión a mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Probando...")
+// crear la conexión y luego para crear el modelo
+import { Schema, model, connect } from "mongoose"
+
+process.loadEnvFile()
+
+const URI_DB = process.env.URI_DB || ""
+
+const connectMongoDb = async () => {
+  try {
+    await connect(URI_DB)
+    console.log("✅ Conectado a MongoDB")
+  } catch (error) {
+    console.error('❌ Error al conectar a MongoDB')
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connectMongoDb()
